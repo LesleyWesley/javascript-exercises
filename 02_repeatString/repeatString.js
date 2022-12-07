8e6cd45dc1787ce9c7c910e3ddd758b539c40a34
lesley_w <lrwesley92@gmail.com>
2022-12-07 16:37:40 -0600

    Added function to repeat string a given number of times and pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
 
+            let newArray = [];
+
+            for (let i = 1; i <= num; i++) {
+                newArray.push(string);
+            }
+
+            return newArray.join("");
+    }
 };
 
 // Do not edit below this line
