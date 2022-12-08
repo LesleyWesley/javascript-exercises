1b250deeb7f83037f038999e21c9d73384a28e6c
lesley_w <lrwesley92@gmail.com>
2022-12-08 13:56:17 -0600

    Wrote function that takes a given string and reverses it.  Passes all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const length = string.length;
 
+                let forwardsArray = string.split("");
+                let backwardsArray = [];
+
+                for (let i = 0; i < length; i++) {
+                    backwardsArray.push(forwardsArray[forwardsArray.length - 1]);
+                    forwardsArray.pop();
+                }
+
+                return backwardsArray.join("");
 };
 
 // Do not edit below this line
