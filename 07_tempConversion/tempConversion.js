06743ca7ba401de12325671f5444b16c13660151
lesley_w <lrwesley92@gmail.com>
2022-12-11 15:50:55 -0600

    Wrote functions to convert celsius to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = ((temp - 32) * (5/9));
+
+  if (Number.isInteger(newTemp)) {
+    return newTemp;
+  } else {
+    return Number(newTemp.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = ((temp * 1.8) + 32);
+
+  if (Number.isInteger(newTemp)) {
+    return newTemp;
+  } else {
+    return Number(newTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
