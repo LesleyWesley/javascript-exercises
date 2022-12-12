ed95000e4cc705735cbb907dafa5946ac4ccf9f2
lesley_w <lrwesley92@gmail.com>
2022-12-11 18:14:30 -0600

    Wrote functions for basic math calculations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,67 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+
+  if (numArray.length < 1) {
+    return 0;
+  } else {
+    let total = numArray[0];
+
+    for (let i = 1; i < numArray.length; i++) {
+      total = total + numArray[i];
+
+    }
+
+    return total;
+  }
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  let total = numArray[0];
 
+  for (let i = 1; i < numArray.length; i++) {
+     total = total * numArray[i];
+   }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+
+  if (num === 0) {
+    return 1;
+  } else {
+  
+      //Creates array
+      let currentNum = num;
+      let array = [];
+
+      while (currentNum > 0) {
+        array.push(currentNum);
+        currentNum--;
+      }
+
+      //Multiplies array items
+
+      let total = array[0];
+
+      for (let i = 1; i < array.length; i++) {
+        total = total * array[i];
+      }
+
+      return total;
+  }
+}
 
 // Do not edit below this line
 module.exports = {
