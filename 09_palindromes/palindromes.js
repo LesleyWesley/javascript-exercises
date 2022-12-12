5263941dee60b2915666bf394e35a00c93e70f05
lesley_w <lrwesley92@gmail.com>
2022-12-12 15:46:56 -0600

    Wrote a function to determine if a given string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //Removes punctuation and spaces from original string
+    const formattedString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{1,}/g, "").toLowerCase();
 
+    const forwardArray = formattedString.split("");
+    let backwardsArray = [];
+
+    for (let i = forwardArray.length-1; i >= 0; i--) {
+      backwardsArray.push(forwardArray[i]);
+    }
+
+    if (backwardsArray.join("") === formattedString) {
+      return true;
+    } else {
+      return false;
+    }
 };
 
 // Do not edit below this line
