cb7a4928f2914469e3fad6e246c589117efdd09e
lesley_w <lrwesley92@gmail.com>
2022-12-12 18:07:39 -0600

    Created function to return the oldest person in an array of people objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    const agesArray = [];
+    let age;
 
+
+    //Gets ages and pushes them to their own array
+    for(let i = 0; i < array.length; i++) {
+      if (array[i].hasOwnProperty("yearOfDeath")) {
+        age = array[i].yearOfDeath - array[i].yearOfBirth;
+      } else {
+        age  = currentYear - array[i].yearOfBirth;
+      }
+
+      agesArray.push(age);
+    }
+    //Finds largest number in ages array
+    let highest = 0;
+    let index;
+
+    for (let i = 0; i < agesArray.length; i++) {
+      if (agesArray[i] > highest) {
+        highest = agesArray[i];
+        index = i;
+      }
+    }
+
+    return array[index];
 };
 
 // Do not edit below this line
